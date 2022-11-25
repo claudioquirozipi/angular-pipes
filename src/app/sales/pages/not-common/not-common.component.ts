468e4b2405175a503d1280d909c5addaf1d3d299
Claudio Quiroz <claudioquirozipi@gmail.com>
2022-11-24 21:14:19 -0500

    pipes not commun

--- src/app/sales/pages/not-common/not-common.component.ts
+++ src/app/sales/pages/not-common/not-common.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-not-common',
@@ -12,4 +13,50 @@ export class NotCommonComponent {
     f: 'invitarla',
     m: 'invitarlo',
   };
+
+  clients: string[] = ['claudio', 'ana', 'josé', 'juan', 'carol'];
+  clientMap = {
+    '=0': 'no tenemos ningún cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    other: 'tenemos # clientes esperando',
+  };
+
+  person = {
+    name: 'Claudio',
+    age: 35,
+    address: 'Lima, Perú',
+  };
+
+  heros = [
+    {
+      name: 'Superman',
+      fly: true,
+    },
+    {
+      name: 'batman',
+      fly: false,
+    },
+    {
+      name: 'Goku',
+      fly: true,
+    },
+  ];
+
+  toggleClientName() {
+    console.log('hola');
+    this.name = this.name === 'Claudio' ? 'Anahis' : 'Claudio';
+    this.gender = this.gender === 'm' ? 'f' : 'm';
+  }
+  removeClient() {
+    console.log('mundo');
+    this.clients.pop();
+  }
+
+  myObservable = interval(1000);
+
+  myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Tenemos data de promesa');
+    }, 3500);
+  });
 }
